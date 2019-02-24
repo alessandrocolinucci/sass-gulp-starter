9254bc81e260cdec52480ed70db054c4ece3628e
Alessandro Colinucci <a.colinucci@gmail.com>
2019-02-24 17:01:32 +0100

    Migrated to Gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,85 +1,30 @@
-const browserSync = require('browser-sync'); // To live reload application
-const gulp = require('gulp'); // To use gulp
-const rename = require('gulp-rename');  // To rename minified files
-const minifyimg = require('gulp-image'); // To minify images
-const sass = require('gulp-sass'); // To compile sass
-const minifycss = require('gulp-clean-css'); // To minify css
-const uglify = require('gulp-uglifyes'); // To minify js ES6
 
-// Serve
-gulp.task('serve', () => {
+const gulp        = require('gulp');
+const sass        = require('gulp-sass');
+const browsersync = require('browser-sync').create();
 
-    browserSync.init({
-        server: './src'
+// COMPILE STYLES
+function compileSASS() {
+    return gulp.src('./src/scss/**/*.scss')
+               .pipe(sass())
+               .pipe(gulp.dest('./src/css'))
+               .pipe(browsersync.stream());
+}
+
+// WATCH UPDATES
+function watch() {
+    browsersync.init({
+        server: {
+            baseDir: './src'
+        },
+        port: 3000
     });
 
-    gulp.watch(['src/scss/*.scss'], ['sass']);
-    gulp.watch(['src/*.html']).on('change', browserSync.reload);
-})
+    gulp.watch('./src/scss/**/*.scss', compileSASS);
+    gulp.watch('./src/**/*.html').on('change', browsersync.reload);
+    gulp.watch('./src/js/**/*.js').on('change', browsersync.reload);
+}
 
 
-// Copy Favicon
-gulp.task('copyfavicon', () => {
-    return gulp.src('src/*.ico')
-               .pipe(gulp.dest('dist'));
-});
-
-// Copy All HTML files
-gulp.task('copyhtml', () => {
-    return gulp.src('src/*.html')
-               .pipe(gulp.dest('dist'));
-});
-
-// Copy all fonts - requires to be .woff2
-gulp.task('copyfonts', () => {
-    return gulp.src('src/fonts/*.woff2')
-               .pipe(gulp.dest('dist/fonts'));
-});
-
-
-// Compile Sass and minify
-
-// Compile sass
-gulp.task('sass', () => {
-    return gulp.src(['src/scss/*.scss'])
-               .pipe(sass().on('error', sass.logError))
-               .pipe(gulp.dest("src/css"))
-               .pipe(browserSync.stream());
-});
-
-// Minify CSS
-gulp.task('minifycss', () => {
-    return gulp.src(['src/scss/*.scss'])
-               .pipe(sass().on('error', sass.logError))
-               .pipe(minifycss())
-               .pipe(rename((path) => {
-                    path.extname = '.min.css';
-                }))
-               .pipe(gulp.dest('dist/css'));
-});
-
-// Minify JS
-gulp.task('minifyjs', () => {
-    return gulp.src(['src/js/*.js'])
-                .pipe(uglify({ 
-                    mangle: false, 
-                    ecma: 6 
-                }))
-                .pipe(rename((path) => {
-                    path.extname = '.min.js';
-                }))
-                .pipe(gulp.dest('dist/js'))
-})
-
-// Optimize images
-gulp.task('imageminify', () => {
-    return gulp.src('src/images/*')
-               .pipe(minifyimg())
-               .pipe(gulp.dest('dist/images'))
-})
-
-// Minify task
-gulp.task('minify', ['copyhtml', 'copyfonts', 'minifycss', 'minifyjs', 'imageminify']);
-
-// Default task
-gulp.task('default', ['serve']);
+exports.compileSASS = compileSASS;
+exports.watch       = watch;
